52d9600388cea9a579f535c4a3b080accadd9cdf
Puji Wahono <puji.wahono@jelajahdata.co.id>
2026-09-02 10:19:51 +0700

    Handle CI and CD
    Handle CI and CD

--- SportsPediaModules/.build/arm64-apple-macosx/debug/SportsPediaModulesPackageTests.derived/runner.swift
+++ SportsPediaModules/.build/arm64-apple-macosx/debug/SportsPediaModulesPackageTests.derived/runner.swift
@@ -15,7 +15,7 @@ public final class SwiftPMXCTestObserver: NSObject {
 
 extension SwiftPMXCTestObserver: XCTestObservation {
     var testOutputPath: String {
-        return "/Users/jds/AndroidStudioProjects/githubpuji/SportPediaJiwoCool/dicoding_capstone/SportsPediaModules/.build/arm64-apple-macosx/debug/testOutput.txt"
+        return "/Users/jds/AndroidStudioProjects/githubpuji/SportPediaJiwoCool/SportsPediaModules/.build/arm64-apple-macosx/debug/testOutput.txt"
     }
 
     private func write(record: any Encodable) {
